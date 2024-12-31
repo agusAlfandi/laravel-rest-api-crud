4fbce236b911e2f185698a9ab91cf4613e739ea1
Agus <agusalfandi8@gmail.com>
2024-12-31 21:01:07 +0700

    API Resource

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $fillable = ['name', 'status'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
